ff80087c7fef2f0ebcdb03f94ccf0b66edb30bf6
Wei Wang <wvw@google.com>
2016-06-03 11:15:31 -0700

    power: Handle launch power hint on/off to boost in accurate time duration
    Current implementation is based on a magic number 5000
    With this and AMS change, the powerHAL can get exact start/end time of launching
    
    Bug: 28220567
    Change-Id: Ief7215bb9f5e02e7c0e612783b79f90ba57dadd1

--- power/utils.c
+++ power/utils.c
@@ -297,6 +297,11 @@ void interaction(int duration, int num_args, int opt_list[])
     }
 }
 
+void release_request(int lock_handle) {
+    if (qcopt_handle && perf_lock_rel)
+        perf_lock_rel(lock_handle);
+}
+
 void perform_hint_action(int hint_id, int resource_values[], int num_resources)
 {
     if (qcopt_handle) {
